d1b8670e6d4327659ab54357a262720847b6edeb
DanewellXalynx <141164215+DanewellXalynx@users.noreply.github.com>
2024-08-12 18:36:29 +0000

    Create main.py
    made the script

---
+++ main.py
@@ -0,0 +1,8 @@
+print("This script is licensed under GNU GPLv3.0")
+
+var = input(str("Is this a JoJo refrence? type an input and see : "))
+
+if var == "DIO": 
+  print("That's a JoJo refrence")
+else
+  print("That's a JoJo refrence")
